3f945bcadbbcc539d0002ae46ebd7c8fa2159275
Peter <52176852+Peter537@users.noreply.github.com>
2025-09-12 18:33:02 +0200

    Create Program.cs

---
+++ CoreMoviePlayer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreMoviePlayer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
